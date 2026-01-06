201eca4eb6530cf1ae327c06e54a102d5d99ce9b
jvn <73175916+JevenDev@users.noreply.github.com>
2026-01-06 02:39:52 -0500

    refactor site to have PSP inspired UI

--- state/selectors.js
+++
@@ -1,5 +0,0 @@
-import { state } from './store.js';
-
-export function isItemInQueue(id){
-  return state.files.some(f=> f.id === id);
-}
--- util/time.js
+++
@@ -1,6 +0,0 @@
-export function secondsToClock(s){
-  s = Math.max(0, s|0);
-  const m = (s/60|0);
-  const ss = (s % 60).toString().padStart(2, "0");
-  return `${m}:${ss}`;
-}
